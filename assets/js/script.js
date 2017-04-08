344409431b5888005a04ef036ff15531134a57e3
FiorellaQA <fiorella_90_17@hotmail.com>
2017-04-07 23:58:29 -0500

    fotos

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,11 @@ var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
    if (currentScroll > lastScrollTop){
-     document.getElementsByTagName('nav-background').style.display="none";
+     document.getElementsByClassName('nav-background')[0].style.display="none";
      document.getElementById("nav-header").classList.remove("header-in");
      document.getElementById("nav-header").style.opacity = 0;
    } else {
-     document.getElementsByTagName('nav-background').style.display="table";
+     document.getElementsByClassName('nav-background')[0].style.display="table";
      document.getElementById("nav-header").classList.add("header-in");
      document.getElementById("nav-header").classList.add("solid");
      document.getElementById("nav-header").style.opacity = 1;
@@ -26,31 +26,48 @@ window.addEventListener("scroll", function(){
 }, false);
 
 /*------------------------------------------------*/
-var coders = [{nombre: "Yessenia",foto:"assets/img/students/1.png"},
-              {nombre: "Fiorella",foto:"assets/img/students/1.png"},
-              {nombre: "Lorem ipsum", foto:"assets/img/students/1.png"}
-              ];
+var coders = [];
+var codersFoto = ["Fiorella Quispe","Lulú Vilchez","Leidy Maldonado","Yessenia Huaman","Miriam Mendoza", "Elizabeth Condori",
+                "Arantza Burga","Grecia Rayme","Janine Vega","Rosario Felix","Daguiana :v", "Jenny Velasquez", "Nadia Cuadros",
+                "Mishel More", "Marilu Llamoca","Mariel García","Fiorella Cisneros", "Geraldine Chauca","Yelitza Choque","Stephanieh Iyagon",
+                "Emma Tapia","Dana Franco","Flor Retamozo", "Nathal Pacheco","Erika Vidal","Katherine Ortega","Brilly Majuan","Flor Cantuta",
+                "Leslie Aavendaño","Cindy Mendoza", "Annia Flores","Betsi Loayza","Ayda Sulca","Milagros Gutierrez","Nakarid Jave", "Angie Condor",
+                "Maia Rojas","Ariana Cabana","Flor Condori", "Mitchell Condori", "Naomi Villanueva","Mary Castillo","Myriam Peralta","Karin Alejo",
+                "Liliana Peña","Ruth Salvador", "Maribel :v","Wendy Reyes","Cynthia Quispe","MariaGrecia Cutipa","Ana Durand","Glisse Jorge","Neiza Nuñez",
+                "Sandra Solorzano"];
+
+for(var i = 1; i < 55; ++i){
+  coders.push({nombre: "Lorem ipsum", foto:"assets/img/students/"+i+".png"});
+}
+
 var containerd=document.getElementById("containerF");
+var template = document.getElementById("template");
 coders.forEach(function(element,index){     
-    for(var prop in element){
-
-      if(prop == "foto"){
-        var src = element[prop];
-        
-        var figure = document.createElement("figure");
-        figure.appendChild(createImage(src));
-
-
-      }
-
-    }
-    containerd.appendChild(figure);
+  var tmp = template.cloneNode(true).getElementsByClassName("foto-alumna")[0];
+  tmp.getElementsByClassName("foto")[0].setAttribute("src", element.foto);
+  tmp.getElementsByClassName("name")[0].innerHTML = codersFoto[index];
+  containerd.append(tmp);
 });
 function createImage(src) {
     var img   = new Image();
     img.src   = src;
   return img; 
 }
+
+
+    // for(var prop in element){
+
+    //   if(prop == "foto"){
+    //     var src = element[prop];
+        
+    //     var figure = document.createElement("figure");
+    //     figure.appendChild(createImage(src));
+
+
+    //   }
+
+    // }
+    // containerd.appendChild(figure);
 // var arrayFotos = [];
 // arrayFotos.push(createImage("",""));
 
